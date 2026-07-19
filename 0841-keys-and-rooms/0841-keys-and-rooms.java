0976e40aec20dd5a63779fd65784d5239d1386d2
aadityaa2023 <adityamishraniet2023@gmail.com>
2026-07-19 20:10:11 +0530

    Time: 0 ms (100%), Space: 46.1 MB (92.19%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,26 +1,24 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         boolean[] vis = new boolean[rooms.size()];
-        Queue<Integer> q = new LinkedList<>();
 
-        vis[0] = true;
-        q.offer(0);
-        while (!q.isEmpty()) {
-            int node = q.poll();
+        dfs(0, rooms, vis);
 
-            for (int key : rooms.get(node)) {
-                if (!vis[key]) {
-                    vis[key] = true;
-                    q.offer(key);
-                }
-            }
-
-        }
         for (int i = 0; i < vis.length; i++) {
             if (vis[i] == false) {
                 return false;
             }
         }
         return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] vis) {
+        vis[room] = true;
+
+        for (int key : rooms.get(room)) {
+            if (!vis[key]) {
+                dfs(key, rooms, vis);
+            }
+        }
     }
 }
