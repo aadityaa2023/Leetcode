027f0e9c4c4eaa822ee56b257616367ee88dab8f
aadityaa2023 <adityamishraniet2023@gmail.com>
2026-07-22 16:51:17 +0530

    Time: 0 ms (100%), Space: 53.6 MB (69.56%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,13 @@
+var searchMatrix = function(matrix, target) {
+     let  m = matrix.length;
+        let  n = matrix[0].length;
+        for(let i =0; i<m; i++){
+            for(let j =0; j<n; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    
+};
