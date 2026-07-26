55213e1c9ff5f1f33cd04273d7062cca66607dfc
aadityaa2023 <adityamishraniet2023@gmail.com>
2026-07-26 12:28:48 +0530

    Time: 3 ms (25.39%), Space: 44 MB (7.28%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String []words = s.split(" ");
+        String ans ="";
+        for(int i =0; i<k; i++){
+            ans = ans+ words[i]+" ";
+        }
+        return ans.trim();
+    }
+}
