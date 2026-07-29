bca7eea85011b22667b24dda5009295e2bd119c1
aadityaa2023 <adityamishraniet2023@gmail.com>
2026-07-29 10:05:54 +0530

    Time: 0 ms (100%), Space: 55.5 MB (67.27%) - LeetHub

---
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.js
@@ -0,0 +1,10 @@
+var evenNumberBitwiseORs = function (nums) {
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0) {
+            res = res | nums[i];
+        }
+    }
+    return res;
+
+};
